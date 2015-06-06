149f8239b4195462e3aea13c7567d4dec1b9d1a9
Floris Padt <floris@fmpadt.nl>
2015-06-06 22:26:27 +0200

    I don't know

--- 00_RProj/00_Global/iSynGeneral.R
+++ 00_RProj/00_Global/iSynGeneral.R
@@ -7,11 +7,18 @@ library(printr    , verbose = FALSE, quietly = TRUE)
 library(xlsx      , verbose = FALSE, quietly = TRUE)
 library(data.table, verbose = FALSE, quietly = TRUE)
 library(RODBC     , verbose = FALSE, quietly = TRUE)
+library(ggplot2   , verbose = FALSE, quietly = TRUE)
+library(reshape2  , verbose = FALSE, quietly = TRUE)
+library(stringr   , verbose = FALSE, quietly = TRUE)
 
 # ---- ReadingData ----
 fGetTable <- function(pTable, pKey, pSystID = "BA1", pClient = "200"){
   
-  l_Table <- paste0(pSystID, "C", pClient, "_", pTable )
+  if(!missing(pSystID)){
+    l_Table <- paste0(pSystID, "C", pClient, "_", pTable )
+  } else {
+    l_Table <- pTable
+  }
 
   # Open COnnection to DataBase
   A2R    <- odbcConnect("ACCESS2R")
@@ -27,27 +34,28 @@ fGetTable <- function(pTable, pKey, pSystID = "BA1", pClient = "200"){
   }
   
   # Get Attributes
-  dfCMT <- sqlQuery(A2R, 
-                    paste("SELECT START, DTIME, RECORDCOUNT", 
-                          "FROM zsDD02_SLCT",
-                          "WHERE ((SYSTEMID=", 
-                          paste0("'", pSystID, "'"),  
-                          ") AND (CLIENT=", 
-                          pClient, ") AND (TABNAME=", 
-                          paste0("'", pTable , "'"), 
-                          "));")) 
-  
-  attr(dtTABLE, "RefreshDate")      <- dfCMT[["START"]]
-  attr(dtTABLE, "RecordsExtracted") <- dfCMT[["RECORDCOUNT"]]
-  
+  if(!missing(pSystID)){
+    dfCMT <- sqlQuery(A2R, 
+                      paste("SELECT START, DTIME, RECORDCOUNT", 
+                            "FROM zsDD02_SLCT",
+                            "WHERE ((SYSTEMID=", 
+                            paste0("'", pSystID, "'"),  
+                            ") AND (CLIENT=", 
+                            pClient, ") AND (TABNAME=", 
+                            paste0("'", pTable , "'"), 
+                            "));")) 
+    
+    attr(dtTABLE, "RefreshDate")      <- dfCMT[["START"]]
+    attr(dtTABLE, "RecordsExtracted") <- dfCMT[["RECORDCOUNT"]]
+  }
   close(A2R)
   
   return(dtTABLE)
 }
 
 # ---- WriteResults ----
-fWriteToSheet <- function(pData, pFileName, pSheetName, pAppend=FALSE){
-  pFileName <- paste0("C:/Users/fpadt/Documents/RW/QAR/60_Results/", pFileName, ".xlsx")
+fWriteToSheet <- function(pData, pPath, pFileName, pSheetName, pAppend=FALSE){
+  pFileName <- paste0(pPath, "/", pFileName, ".xlsx")
   write.xlsx(pData, 
              file = pFileName, sheetName= pSheetName, 
              col.names=TRUE, row.names=FALSE, append=pAppend, showNA=TRUE)
